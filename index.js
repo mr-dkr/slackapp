8be67390660029284314e2f0848efbfe173fd018
mr-dkr <45731096+mr-dkr@users.noreply.github.com>
2024-02-26 11:49:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const axios = require('axios');
+const { App } = require('@slack/bolt');
+const signingSecret = process.env['SLACK_SIGNING_SECRET']
+const botToken = process.env['SLACK_BOT_TOKEN']
+const app = new App({
+    signingSecret: signingSecret,
+    token: botToken,
+});
+(async () => {
+    // Start the app
+    await app.start(process.env.PORT || 12000);
+    app.message('quote', async ({ message, say }) => {
+        let resp = await axios.get(`https://api.quotable.io/random`);
+        console.log(message);
+        const quote = resp.data.content;
+        await say(`Hello, <@${message.user}>, ${quote}`);
+    });
+    console.log('Bolt app is running on 12000!');
+})();
